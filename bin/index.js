f36b546f94b0f21ac68ff65a01ee1594d4bdd0ae
techxplorer <corey@techxplorer.com>
2025-08-24 12:25:50 +0930

    Update the tool to run the command
    Including testing the tool on my content

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,12 @@
 #!/usr/bin/env node
 
+import { existsSync } from 'node:fs';
+
+import chalk from 'chalk';
 import { Command } from "commander";
 import prettyMS from "pretty-ms";
 
+import Migrate from "../src/commands/migrate.js";
 import PackageUtils from "../src/utils/package-utils.js";
 
 const startTime = process.hrtime.bigint();
@@ -16,6 +20,7 @@ async function run() {
   const program = new Command();
   const appPackage = new PackageUtils();
   program
+    .name( "verso-recto" )
     .version( appPackage.getVersion() )
     .description( appPackage.getDescription() )
     .showHelpAfterError( "(add --help for additional information)" )
@@ -24,6 +29,27 @@ async function run() {
       `\nMore info: ${ appPackage.getHomepage() }\nVersion: ${ appPackage.getVersion() }`
     );
 
+  // Add the arguments for the Migrate command to the program.
+  program
+    .argument( "<verso>", "source directory" )
+    .argument( "<recto>", "destination directory" )
+    .action( async( verso, recto ) => {
+
+      console.log( `${program.name()} - ${program.version()}\n` );
+
+      if (existsSync( verso ) === false ) {
+        console.error( `${ chalk.red( "Error:") } verso path must exist`)
+        process.exit(1);
+      }
+
+      if( existsSync( recto ) === false ) {
+        console.error( `${ chalk.red( "Error:") } recto path must exist` );
+        process.exit(1);
+      }
+
+      const migrateCommand = new Migrate( verso, recto );
+      await migrateCommand.run()
+    } )
 
   // Parse the command line parameters.
   await program.parseAsync( process.argv );
